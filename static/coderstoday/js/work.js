0a9ffccf4615009ecdad07894a1f6ba2988c1e69
abhishek <munjalabhishek1996@gmail.com>
2021-06-13 11:11:22 +0530

    Added our first app to the portfolio and made some sections full height on the home page

--- static/coderstoday/js/work.js
+++ static/coderstoday/js/work.js
@@ -2,46 +2,19 @@ const imgContainer = document.querySelector("#img-container");
 console.log(imgContainer);
 const imgUrl = [
     {
-        src: "https://source.unsplash.com/dTRix5MpDR0",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/OQTjr9yl9xU",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/QerZtB_ufKs",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/e59Y6vqbL7Y",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/C6kSpxmKCWQ",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/RacQL72LxoM",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/RacQL72LxoM",
-        alt: "hi",
-    },
-    {
-        src: "https://source.unsplash.com/RacQL72LxoM",
-        alt: "hi",
-    },
+        src: "../static/coderstoday/images/work/anandcolddrinks.svg",
+        alt: "Coders Today",
+    }
 ];
 imgUrl.forEach((element) => {
     let imgHtml = `
     <div class="column is-3">
-    <img
-        class="container-img"
-        src="${element.src}"
-        alt="${element.alt}"
-    />
+        <img
+            class="container-img"
+            src="${element.src}"
+            alt="${element.alt}"
+        />
+        <div class="caption">Product Booking App</div>
     </div>
     `;
     imgContainer.insertAdjacentHTML("beforeend", imgHtml);
